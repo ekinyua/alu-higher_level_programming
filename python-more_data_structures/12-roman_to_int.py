de48511dc70aea2f450ffc7424c927f27ff7973b
ekinyua <e.kinyua@alustudent.com>
2022-11-03 00:34:03 -0700

    error fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,16 +2,15 @@
 # function def roman_to_int(roman_string):
 # that converts a Roman numeral to an integer.
 
-
 def roman_to_int(roman_string):
-    if roman_string is None or type(roman_string) is not str:
+    """Converts a Roman numeral to an integer"""
+    if not roman_string or type(roman_string) is not str:
         return 0
-    int_sum = 0
-    converter = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
-    for current, next in zip(roman_string, roman_string[1:]):
-        if converter[current] >= converter[next]:
-            int_sum += converter[current]
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+    for i in range(len(roman_string)):
+        if i > 0 and roman[roman_string[i]] > roman[roman_string[i - 1]]:
+            total += roman[roman_string[i]] - 2 * roman[roman_string[i - 1]]
         else:
-            int_sum -= converter[current]
-    int_sum += converter[roman_string[-1]]
-    return 
+            total += roman[roman_string[i]]
+    return total 

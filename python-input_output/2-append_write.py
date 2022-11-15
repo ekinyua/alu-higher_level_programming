c23c9f3b653d808490de2af5644eadd11e3a11b1
ekinyua <e.kinyua@alustudent.com>
2022-11-15 12:35:42 -0800

    edit

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""
+    define a function 'append_write'
+"""
 
 def append_write(filename="", text=""):
-    with open(filename, 'a+') as f:
+
+    with open(filename, 'a', encoding='utf-8') as f:
         return f.write(text)

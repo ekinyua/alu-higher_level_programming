d5856c4a7fc48133885fe8c9af8d276e37286c5d
ekinyua <e.kinyua@alustudent.com>
2022-11-15 12:30:52 -0800

    changes

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-append_write = __import__('2-append_write').append_write
 
-nb_characters_added = append_write("file_append.txt", "This School is so cool!\n")
-print(nb_characters_added)
+def append_write(filename="", text=""):
+    with open(filename, 'a+') as f:
+        return f.write(text)
